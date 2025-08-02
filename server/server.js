928a6aa76d6a29f19ac15e7817a991812c4aac72
Abhi Rajput <162717567+abhirajput-19@users.noreply.github.com>
2025-08-02 13:07:02 +0530

    Configured server to use PORT from .env

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) => {
 app.use("/api/admin", adminRouter);
 app.use("/api/blog", blogRouter);
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
